80bacd9c4f84d95c682cedfec35f4770d5732796
BranY <yangwen_jia@163.com>
2016-12-18 14:11:49 +0800

    first commit

--- src/main/scala/edu/nju/pasalab/test/testEncoder.scala
+++ src/main/scala/edu/nju/pasalab/test/testEncoder.scala
@@ -5,8 +5,6 @@ import java.util
 import edu.nju.pasalab.Person
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.{Encoders, SparkSession}
-
-import scala.collection.Iterator
 /**
   * Created by YWJ on 2016.11.19.
   * Copyright (c) 2016 NJU PASA Lab All rights reserved.
@@ -16,11 +14,8 @@ object testEncoder {
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
-    //val lo = LoggerFactory.getLogger(test.getClass)
     val spark = SparkSession.builder().master("local[4]")
       //.config("spark.kryo.registrator", "edu.nju.pasalab.mt.wordAlignment.usemgiza.MyKryoRegistrator")
-      //.config("spark.storage.memoryFraction", args(8))
-      //.config("spark.shuffle.memoryFraction", args(9))
       .appName("test").getOrCreate()
 
 
@@ -32,45 +27,29 @@ object testEncoder {
 
     //val strEncoder = Encoders.product[String]
 
-    val ds  = spark.read.text("data/data.txt").as[String]
-    val gp =ds.flatMap(e => e.split("\\s+")).map(x => (x, 1.0)).groupByKey(elem => elem._1)
-    val gp1 = gp.mapGroups((k, v) => (k, v.map(x => x._2).sum))
-    gp1.show()
+    val ds  = spark.read.text("data/test1k.txt").as[String]
+//    val gp =ds.flatMap(e => e.split("\\s+")).map(x => (x, 1.0)).groupByKey(elem => elem._1)
+//    val gp1 = gp.mapGroups((k, v) => (k, v.map(x => x._2).sum))
+//    gp1.show()
 
-    val sample = ds.flatMap(e => e.split("\\s+")).groupByKey(x => x)
-      .count()
-
+    val sample = ds.flatMap(e => e.split("\\s+")).groupByKey(x => x).mapGroups((k, v) => (k ,v.length)).as("A")
 
     import spark.implicits._
-    val ta = sample.as("A")
     val tb = sample.map(x => (x._1, (x._2 + 10)/ 5.0)).as("B")
-    val ja = ta.joinWith(tb, $"value" === $"value","inner")
+    val ja = sample.joinWith(tb, $"A._1" === $"B._1","inner").map(elem => (elem._1._1, (elem._1._2, elem._2._2)))
 
-    ja.show()
-//   import org.apache.spark.util.collection.AppendOnlyMap
-//    val  xx = new AppendOnlyMap[String, Int]()
-//    /** **/
-//    val encoder2 = Encoders.tuple(Encoders.INT, Encoders.STRING)
-//    //val xxx = Encoders.kryo[Person]
-//    println(encoder2.clsTag + " *** " + encoder2.schema)
-//    val encoder  = Encoders.product[Person]
-//    println(encoder.schema)
-//    val t = spark.createDataset(list)(encoder)
-//
-//
-//    t.show()
-//
-//    /** **/
-//    val df = Seq(("a", 10), ("a", 20), ("b", 1), ("b", 2), ("c", 1)).toDS()
-//    val da = df.groupByKey(v => (v._1, "word"))
-//
-//    val xy = da.mapGroups{case (g, iter) =>(g._1, iter.map(_._2).sum)}
-//
-//    xy.show()
-//    val res = da.flatMapGroups {
-//      case (g, iter) => Iterator(g._1 + " " + iter.map(_._2).sum.toString)
-//    }
-//    res.show()
+    ja.show(100)
+
+    val encoder2 = Encoders.tuple(Encoders.INT, Encoders.STRING)
+    //val xxx = Encoders.kryo[Person]
+    println(encoder2.clsTag + " *** " + encoder2.schema)
+    val encoder  = Encoders.product[Person]
+    println(encoder.schema)
+    val t = spark.createDataset(list)(encoder)
+
+
+    t.show()
+
 
   }
 }
