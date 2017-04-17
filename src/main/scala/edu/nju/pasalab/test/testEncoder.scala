6ae686f21eed668ab1b00b24c5909958e97a9d0c
BranY <yangwen_jia@163.com>
2017-04-17 17:13:06 +0800

    indexed

--- src/main/scala/edu/nju/pasalab/test/testEncoder.scala
+++ src/main/scala/edu/nju/pasalab/test/testEncoder.scala
@@ -15,6 +15,7 @@ object testEncoder {
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
     val spark = SparkSession.builder().master("local[4]")
+      .config("spark.kryo.registrationRequired", "true")
       //.config("spark.kryo.registrator", "edu.nju.pasalab.mt.wordAlignment.usemgiza.MyKryoRegistrator")
       .appName("test").getOrCreate()
 
@@ -26,32 +27,29 @@ object testEncoder {
     list.add(new Person("B", 2))
 
     val ds  = spark.read.textFile("data/data.txt")
-      //.as[String]
-//    val gp =ds.flatMap(e => e.split("\\s+")).map(x => (x, 1.0)).groupByKey(elem => elem._1)
-//    val gp1 = gp.mapGroups((k, v) => (k, v.map(x => x._2).sum))
-//    gp1.show()
+
 
     val sample = ds.flatMap(e => e.split("\\s+")).groupByKey(x => x).mapGroups((k, v) => (k ,v.length)).alias("A")
 
     sample.show(100)
 
     import spark.implicits._
-    spark.sqlContext.sql("set spark.sql.caseSensitive=false")
+    //spark.sqlContext.sql("set spark.sql.caseSensitive=false")
     val tb = sample.map(x => (x._1, (x._2 + 10)/ 5.0)).alias("B")
     println(tb.schema)
     val ja = sample.joinWith(tb, $"A._1" === $"B._1", "inner").map(elem => (elem._1._1, (elem._1._2, elem._2._2)))
 
     ja.show(20)
 
-    val encoder2 = Encoders.tuple(Encoders.INT, Encoders.STRING)
-    //val xxx = Encoders.kryo[Person]
-    println(encoder2.clsTag + " *** " + encoder2.schema)
-    val encoder  = Encoders.product[Person]
-    println(encoder.schema)
-    val t = spark.createDataset(list)(encoder)
+//    val encoder2 = Encoders.tuple(Encoders.INT, Encoders.STRING)
+//    //val xxx = Encoders.kryo[Person]
+//    println(encoder2.clsTag + " *** " + encoder2.schema)
+//    val encoder  = Encoders.product[Person]
+//    println(encoder.schema)
+//    val t = spark.createDataset(list)(encoder)
 
 
-    t.show()
+    //t.show()
 
 
   }
